475fa6b7997a0404fb1f530bc57e845f6e36bba9
Andrei Malyhin <ankoma22@gmail.com>
2018-11-05 19:33:13 +0300

    added empty proj

---
+++ OnboardingExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnboardingExample
+//
+//  Created by Andrei Malyhin on 11/5/18.
+//  Copyright © 2018 Ankoma Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
